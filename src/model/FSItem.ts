b598aacef09e6c1d28a9aefb7f3dc9cc3e2ad637
sebastiandero <sebastian.dero@outlook.com>
2018-05-22 15:10:19 +0200

    model classes

---
+++ src/model/FSItem.ts
@@ -0,0 +1,11 @@
+class FSItem {
+    name: String
+    path: String
+    isRemote: boolean
+
+    constructor(name: String, path: String, isRemote: boolean) {
+        this.name = name
+        this.path = path
+        this.isRemote = isRemote
+    }
+}
